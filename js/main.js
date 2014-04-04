ab123f6755be3acd75c408629abeb0047d363234
Josh Kramer <josh.kramer@effectiveui.com>
2014-04-04 14:30:31 -0400

    Add position of center of ball and proper screen width/height

--- js/main.js
+++ js/main.js
@@ -12,8 +12,8 @@ window.Balls = (function() {
     initialize: function() {
       // get width/height
       this.screen = {
-        height: window.screen.availHeight,
-        width: window.screen.availWidth
+        height: window.innerHeight,
+        width: window.innerWidth
       };
       this.rootEle = document.getElementById('view');
 
@@ -37,19 +37,36 @@ window.Balls = (function() {
 
 
   var Ball = function(radius) {
-
+    this.radius = radius;
+    this.position = {
+      x: 0,
+      y: 0
+    };
     this.ele = document.createElement('div');
     this.ele.setAttribute('style', 'position:absolute;width:'+(radius*2)+'px;height:'+(radius*2)+'px;background:red; border-radius:'+radius+'px');
   };
 
   Ball.prototype = {
     moveBy: function(x, y) {
-      this.ele.style.top = (parseInt(this.ele.style.top) + y) + 'px';
-      this.ele.style.left = (parseInt(this.ele.style.left) + x) + 'px';
+      this.moveTo(this.position.x + x, this.position.y + y);
     },
     moveTo: function(x, y) {
-      this.ele.style.top = y + 'px';
-      this.ele.style.left = x + 'px';
+      var nextX = x, nextY = y;
+      if(y > Balls.screen.height - this.radius) {
+        nextY = Balls.screen.height - this.radius;
+      } else if (y < this.radius) {
+        nextY = this.radius;
+      }
+      if(x > Balls.screen.width - this.radius) {
+        nextX = Balls.screen.width - this.radius;
+      } else if (x < this.radius) {
+        nextX = this.radius;
+      }
+      this.ele.style.top = (nextY-this.radius) + 'px';
+      this.ele.style.left = (nextX-this.radius) + 'px';
+      this.position.x = nextX;
+      this.position.y = nextY;
+
     },
     getEle: function() {
       return this.ele;
